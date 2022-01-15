104e87880d8fd0a152e376ad634dfab3c1f3f343
Dinesh Rout <rddinesh779@gmail.com>
2022-01-15 22:52:08 +0530

    protected private routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Profile from './pages/profile/Profile';
 import Search from './pages/search/Search';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
+import { AuthContextProvider } from './context/AuthContext';
 import { AuthContext } from './context/AuthContext';
 import { useContext } from 'react';
+import People from './pages/people/People';
 
 function App() {
-  const { user } = useContext(AuthContext);
   return (
-    <Router>
+    <AuthContextProvider>
       <Routes>
-        <Route path='/' element={user ? <Home /> : <Login />} />
-        <Route path='/profile/:id' element={<Profile />} />
-        <Route path='/search' element={<Search />} />
-        {/* <Route path='/people' element={<Poeple />} /> */}
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+        <Route
+          path='/'
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='people'
+          element={
+            <PrivateRoute>
+              <People />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='profile/:id'
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='search'
+          element={
+            <PrivateRoute>
+              <Search />
+            </PrivateRoute>
+          }
+        />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+        <Route path='*' element={<Navigate to={'/login'} />} />
       </Routes>
-    </Router>
+    </AuthContextProvider>
   );
 }
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useContext(AuthContext);
+  return user ? children : <Navigate to='/login' />;
+};
 
 export default App;
