ab19d926e493c47cf0b090cc03ca7a5a34201b4e
Dinesh Rout <rddinesh779@gmail.com>
2022-01-15 23:21:22 +0530

    refactor

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,33 +1,39 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 import AuthReducer from './AuthReducer';
 
 const INITIAL_STATE = {
-  user: {
-    _id: '61dd2413cee9934ed925a6b2',
-    username: 'Dinesh',
-    email: 'din@gmail.com',
-    password: '$2b$10$7zvM4AUvCA1qjh8411ptMOCGcWlGqZWbSZlOovT4Bc/BeoRi1gxKi',
-    profilePic: '',
-    coverPic: '',
-    followers: ['61dd2424cee9934ed925a6b5', '61dee9828af8ee51c9d1d837'],
-    following: ['61dd2424cee9934ed925a6b5', '61dee9828af8ee51c9d1d837'],
-    isAdmin: false,
-    createdAt: '2022-01-11T06:30:43.080Z',
-    updatedAt: '2022-01-14T08:32:12.396Z',
-    __v: 0,
-  },
+  user: JSON.parse(localStorage.getItem('user')) || null,
   isFetching: false,
   error: false,
 };
+
+// const INITIAL_STATE = {
+//   user: {
+//     _id: '61dd2413cee9934ed925a6b2',
+//     username: 'Dinesh',
+//     email: 'din@gmail.com',
+//     password: '$2b$10$7zvM4AUvCA1qjh8411ptMOCGcWlGqZWbSZlOovT4Bc/BeoRi1gxKi',
+//     profilePic: '',
+//     coverPic: '',
+//     followers: ['61dd2424cee9934ed925a6b5'],
+//     following: ['61dd2424cee9934ed925a6b5'],
+//     isAdmin: false,
+//     createdAt: '2022-01-11T06:30:43.080Z',
+//     updatedAt: '2022-01-14T09:28:18.457Z',
+//     __v: 0,
+//   },
+//   isFetching: false,
+//   error: false,
+// };
 
 export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
-  // useEffect(() => {
-  //   JSON.stringify(localStorage.setItem('user', state.user));
-  // }, [state.user]);
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(state.user));
+  }, [state.user]);
 
   return (
     <AuthContext.Provider
