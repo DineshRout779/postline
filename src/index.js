d31357c9d2d9d60fd1022c5ba825ba12f319c566
Dinesh Rout <49337110+DineshRout779@users.noreply.github.com>
2022-01-17 12:44:49 +0530

    Replaced from browserrouter to hashrouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 // import { AuthContextProvider } from './context/AuthContext';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
