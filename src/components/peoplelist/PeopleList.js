b5598722c6d5250afb9f0cc3b84719e9e7e83cec
Dinesh Rout <rddinesh779@gmail.com>
2022-02-10 19:21:05 +0530

    Minor Refactoring

--- src/components/peoplelist/PeopleList.js
+++ src/components/peoplelist/PeopleList.js
@@ -2,16 +2,17 @@ import { Link } from 'react-router-dom';
 import Avatar from '../avatar/Avatar';
 import './peoplelist.css';
 
-const PeopleList = ({ list, max, message }) => {
+const PeopleList = ({ list, max, message, title }) => {
   return (
     <div>
+      <h3>{title ? title : ''}</h3>
       <ul>
         {list.length > 0 ? (
           list.slice(0, max ? max : list.length).map((p) => {
             return (
               <li className='flex justify-between align-center' key={p._id}>
                 <div className='flex align-center'>
-                  <Avatar s='50' />
+                  <Avatar s='50' profilePic={p.profilePic} />
                   <Link to={`/profile/${p._id}`}>
                     <h4>{p.username}</h4>
                   </Link>
