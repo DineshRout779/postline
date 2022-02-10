30744e67a79505a2a0b559df143324b4ed18b246
Dinesh Rout <rddinesh779@gmail.com>
2022-02-10 21:39:47 +0530

    code refactor

--- src/pages/notfound/NotFound.js
+++ src/pages/notfound/NotFound.js
@@ -3,9 +3,6 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <div className='container full-height flex justify-center align-center flex-column'>
-      <div className='svg'>
-        <img src='./assets/images/404.svg' alt='' />
-      </div>
       <h1 className='text-big'>404</h1>
       <p>Page not found</p>
       <Link to={'/login'}>Login</Link>
