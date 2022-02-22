62478c7f1d583d7986474619b1a51aa86c51bd2b
Dinesh Rout <rddinesh779@gmail.com>
2022-02-22 22:20:54 +0530

    code refactor

--- src/components/fileupload/FileUpload.js
+++ src/components/fileupload/FileUpload.js
@@ -1,23 +1,37 @@
-const FileUpload = ({ onDone, img }) => {
-  function handleChange(e) {
-    // get the files
-    let file = e.target.files[0];
-    let reader = new FileReader();
-    // Convert the file to base64 text
-    reader.readAsDataURL(file);
+import imageCompression from 'browser-image-compression';
 
-    // on reader load somthing...
-    reader.onload = () => {
-      // Make a fileInfo Object
-      let fileInfo = {
-        name: file.name,
-        type: file.type,
-        size: Math.round(file.size / 1000) + ' kB',
-        base64: reader.result,
-        file: file,
-      };
-      onDone(fileInfo);
+const FileUpload = ({ onDone, img }) => {
+  async function handleChange(e) {
+    // get the files
+    let imgFile = e.target.files[0];
+
+    const options = {
+      maxSizeMB: 1,
+      maxWidthOrHeight: 1920,
+      useWebWorker: true,
     };
+
+    try {
+      const compressedImg = await imageCompression(imgFile, options);
+      let reader = new FileReader();
+      // Convert the file to base64 text
+      reader.readAsDataURL(compressedImg);
+
+      // on reader load somthing...
+      reader.onload = () => {
+        // Make a fileInfo Object
+        let fileInfo = {
+          name: compressedImg.name,
+          type: compressedImg.type,
+          size: Math.round(compressedImg.size / 1000) + ' kB',
+          base64: reader.result,
+          file: compressedImg,
+        };
+        onDone(fileInfo);
+      };
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <input
