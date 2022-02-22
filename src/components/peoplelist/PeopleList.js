bcd89bc1b916cf32895af2d3e7e835d452a726b2
Dinesh Rout <rddinesh779@gmail.com>
2022-02-22 22:21:54 +0530

    refactor

--- src/components/peoplelist/PeopleList.js
+++ src/components/peoplelist/PeopleList.js
@@ -1,21 +1,85 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
 import Avatar from '../avatar/Avatar';
 import './peoplelist.css';
 
-const PeopleList = ({ list, max, message, title }) => {
+const url = process.env.REACT_APP_API_URL;
+
+const PeopleList = ({ list, max, message, title = 'people' }) => {
+  const [user, setUser] = useState(null);
+
+  const { user: currentUser } = useContext(AuthContext);
+
+  const followUser = async (followId) => {
+    try {
+      const res = await axios.put(
+        `${url}/users/follow/${user._id}`,
+        {
+          followId,
+        },
+        { headers: { Authorization: `Bearer ${currentUser.token}` } }
+      );
+      console.log(res.data);
+    } catch (err) {
+      console.log(err.response);
+    }
+  };
+
+  const unFollowUser = async (unfollowId) => {
+    try {
+      const res = await axios.put(
+        `${url}/users/unfollow/${currentUser._id}`,
+        {
+          unfollowId,
+        },
+        { headers: { Authorization: `Bearer ${currentUser.token}` } }
+      );
+      console.log(res.data);
+    } catch (err) {
+      console.log(err.response);
+    }
+  };
+
+  useEffect(() => {
+    const fethcUser = async () => {
+      try {
+        const res = await axios.get(`${url}/users/${currentUser._id}`, {
+          headers: { Authorization: `Bearer ${currentUser.token}` },
+        });
+        setUser(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fethcUser();
+  }, [currentUser]);
+
   return (
-    <div>
-      <h3>{title ? title : ''}</h3>
+    <>
       <ul className='people-list'>
         {list.length > 0 ? (
           list.slice(0, max ? max : list.length).map((p) => {
             return (
               <li className='flex justify-between align-center' key={p._id}>
-                <div className='flex align-center'>
-                  <Avatar s='50' profilePic={p.profilePic} />
-                  <Link to={`/profile/${p._id}`}>
-                    <h4>{p.username}</h4>
-                  </Link>
+                <Avatar s='50' profilePic={p.profilePic} />
+                <Link to={`/profile/${p._id}`}>
+                  <h4>{p.username}</h4>
+                </Link>
+                <div className='ml-auto'>
+                  <button
+                    className='btn'
+                    onClick={
+                      user && !user.following.includes(p._id)
+                        ? () => followUser(p._id)
+                        : () => unFollowUser(p._id)
+                    }
+                  >
+                    {user && !user.following.includes(p._id)
+                      ? 'Follow'
+                      : 'Unfollow'}
+                  </button>
                 </div>
               </li>
             );
@@ -24,8 +88,9 @@ const PeopleList = ({ list, max, message, title }) => {
           <p>{message ? '' : 'No users found!'}</p>
         )}
       </ul>
-      {max && <Link to={'people'}>View more</Link>}
-    </div>
+      {list.length > max && <Link to={'people'}>View more</Link>}
+      <br />
+    </>
   );
 };
 
