80b8bc98c8a95e842995148080d5d8920f2f8e30
Dinesh Rout <rddinesh779@gmail.com>
2022-02-26 21:44:42 +0530

    added post apis

--- src/api/post-api.js
+++ src/api/post-api.js
@@ -2,43 +2,61 @@ import axios from 'axios';
 
 const url = process.env.REACT_APP_API_URL;
 
+const getMyPosts = async (id) => {
+  try {
+    const res = await axios.get(`${url}/posts/myposts/${id}`);
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getTimelinePosts = async (id) => {
+  try {
+    const res = await axios.get(`${url}/posts/feed/${id}`);
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
 const createPost = async (post, currentUser) => {
   try {
     const res = await axios.post(`${url}/posts/${currentUser._id}`, post);
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updatePost = async (postId, userId, token, desc) => {
+  try {
+    const res = await axios.put(
+      `${url}/posts/${postId}/${userId}`,
+      {
+        desc,
+      },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deletePost = async (postId, userId, token) => {
+  try {
+    const res = await axios.delete(
+      `${url}/posts/${postId}/${userId}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      },
+      { data: { postedBy: userId } }
+    );
     return res;
   } catch (err) {
     console.log(err.response);
   }
 };
 
-// const updatePost = async (post, desc) => {
-//   try {
-//     await axios.put(
-//       `${url}/posts/${post._id}`,
-//       {
-//         userId: user._id,
-//         desc,
-//       },
-//       { headers: { Authorization: `Bearer ${user.token}` } }
-//     );
-//     setPosts(posts.map((p) => (p._id === post._id ? { ...p, desc } : p)));
-//     return true;
-//   } catch (err) {
-//     console.log(err.response);
-//   }
-// };
-
-// const deletePost = async (post) => {
-//   try {
-//     await axios.delete(`${url}/posts/${post._id}`, {
-//       data: { userId: user._id },
-//       headers: { Authorization: `Bearer ${user.token}` },
-//     });
-//     setPosts(posts.filter((p) => p._id !== post._id));
-//     return true;
-//   } catch (err) {
-//     console.log(err.response);
-//   }
-// };
-
-export { createPost };
+export { getMyPosts, getTimelinePosts, createPost, updatePost, deletePost };
