de3405342b3ad25d58ed5a5c9285280c8a5af9a1
Dinesh Rout <rddinesh779@gmail.com>
2022-02-26 14:39:11 +0530

    refactor - gathered all user apis

--- src/api/user-api.js
+++ src/api/user-api.js
@@ -2,38 +2,97 @@ import axios from 'axios';
 
 const url = process.env.REACT_APP_API_URL;
 
-const getUser = async (id, token, currentUser) => {
+const getUser = async (id, token) => {
   try {
     const res = await axios.get(`${url}/users/${id}`, {
-      headers: { Authorization: `Bearer ${currentUser.token}` },
+      headers: { Authorization: `Bearer ${token}` },
     });
-    return res.data;
-  } catch (error) {}
-};
-
-const findPeople = async (params, token, signal, user) => {
-  try {
-    let res = await axios.get(`${url}/users/findpeople/${user._id}`, {
-      headers: { Authorization: `Bearer ${user.token}` },
-    });
-    return res.data;
-  } catch (err) {
-    console.log(err);
+    return res;
+  } catch (error) {
+    return error;
   }
 };
 
-const fetchAllFollowing = async (user) => {
+const findPeople = async (id, token) => {
   try {
-    let res = await axios.get(`${url}/users/${user._id}`, {
-      headers: { Authorization: `Bearer ${user.token}` },
+    let res = await axios.get(`${url}/users/findpeople/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
     });
-    return res.data;
-  } catch (err) {
-    console.log(err);
+    return res;
+  } catch (error) {
+    return error;
   }
 };
 
-export { getUser, findPeople, fetchAllFollowing };
+const updateUser = async (id, data, token) => {
+  try {
+    const res = await axios.put(
+      `${url}/users/${id}`,
+      {
+        userId: id,
+        coverPic: data.coverText,
+        username: data.username,
+        email: data.email,
+        profilePic: data.profilePic,
+      },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteUser = async () => {};
+
+const followUser = async (id, followId, token) => {
+  try {
+    const res = await axios.put(
+      `${url}/users/follow/${id}`,
+      {
+        followId,
+      },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+const unFollowUser = async (id, unfollowId, token) => {
+  try {
+    const res = await axios.put(
+      `${url}/users/unfollow/${id}`,
+      {
+        unfollowId,
+      },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+const searchUser = async (searchTerm) => {
+  try {
+    const res = await axios.get(`${url}/users?username=${searchTerm}`);
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+export {
+  getUser,
+  findPeople,
+  updateUser,
+  deleteUser,
+  followUser,
+  unFollowUser,
+  searchUser,
+};
 
 // headers: {
 //     Authorization: 'Bearer ' + token,
